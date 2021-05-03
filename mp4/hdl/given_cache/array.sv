9589f215ef4f00ff2c43a3a0d04bdbd60d38b2e4
Naveen Nathan <nnathan2@illinois.edu>
2021-05-02 22:04:37 -0500

    expanded L1 cache

--- mp4/hdl/given_cache/array.sv
+++ mp4/hdl/given_cache/array.sv
@@ -3,14 +3,14 @@ module array #(parameter width = 1)
 (
   input clk,
   input logic load,
-  input logic [2:0] rindex,
-  input logic [2:0] windex,
+  input logic [3:0] rindex,
+  input logic [3:0] windex,
   input logic [width-1:0] datain,
   output logic [width-1:0] dataout
 );
 
 //logic [width-1:0] data [2:0] = '{default: '0};
-logic [width-1:0] data [8];
+logic [width-1:0] data [16];
 initial begin
   data[0] = 0;
   data[1] = 0;
@@ -20,6 +20,14 @@ initial begin
   data[5] = 0;
   data[6] = 0;
   data[7] = 0;
+  data[8] = 0;
+  data[9] = 0;
+  data[10] = 0;
+  data[11] = 0;
+  data[12] = 0;
+  data[13] = 0;
+  data[14] = 0;
+  data[15] = 0;
 end
 
 always_comb begin
